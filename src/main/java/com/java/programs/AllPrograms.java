ada2d3d16befcb645f6af0afc93bacab04b296ab
karunachalam <karunachalam@pch.com>
2018-04-04 07:15:27 -0400

    Adding updates

--- src/main/java/com/java/programs/AllPrograms.java
+++ src/main/java/com/java/programs/AllPrograms.java
@@ -1,37 +1,56 @@
 package com.java.programs;
 
+import org.testng.annotations.Test;
+
 public class AllPrograms {
 
-//			Print prime numbers?  
-//			Basic Java program to find area and perimeter of circle
-//			Print 1 to 10 without using loops  
-//			Print numbers in pyramid shape?  
-//			Add 2 Matrices
-//			Basic Java Example Program to check even or add
-//			What happens if we place return statement in try catch blocks 
-//			Write a java program to convert binary to decimal 
-//			Java Program to convert Decimal to Binary
-//			Interfaces allows constructors? 
-//			Can we create private constructor in java
-//			Java interview Questions on main() method  
-//			Explain java data types with example programs 
-//			Swap two numbers without using third variable in java 
-//			Find sum of digits in java 
-//			Check Even or Odd without using modulus and division  
-//			String Reverse Without using String API 
-//			Check string is palindrome or not?
-//			Reverse a number in java?
-//			Fibonacci series with Recursive?
-//			Fibonacci series without using Recursive?
-//			Sort the String using string API?
-//			Sort the String without using String API?
-//			How to find largest element in an array with index and value ?
-//			Sort integer array using bubble sort in java?
-//			Check armstrong number or not?
-//			Multiply 2 Matrices
-//			Explain return type in java 
-//			Can we call Sub class methods using super class object? 
-//			Can we Override private methods ? 
+	// Print prime numbers?
+	@Test(enabled = true)
+	public void printPrimeNumbers() {
+		//Prime numbers below 100
+		int flag = 0;
+		System.out.println(1);
+		for(int i=2; i<100; i++){
+			for(int j=1; j<=i; j++){
+				if(i%j == 0){
+					flag++;
+				}
+			}
+			if(flag == 2)
+				System.out.println(i);
+			flag = 0; // Its important to reset flag value to zero at any cost.
+		}
+
+	}
 	
-	
+	// Basic Java program to find area and perimeter of circle
+	// Print 1 to 10 without using loops
+	// Print numbers in pyramid shape?
+	// Add 2 Matrices
+	// Basic Java Example Program to check even or add
+	// What happens if we place return statement in try catch blocks
+	// Write a java program to convert binary to decimal
+	// Java Program to convert Decimal to Binary
+	// Interfaces allows constructors?
+	// Can we create private constructor in java
+	// Java interview Questions on main() method
+	// Explain java data types with example programs
+	// Swap two numbers without using third variable in java
+	// Find sum of digits in java
+	// Check Even or Odd without using modulus and division
+	// String Reverse Without using String API
+	// Check string is palindrome or not?
+	// Reverse a number in java?
+	// Fibonacci series with Recursive?
+	// Fibonacci series without using Recursive?
+	// Sort the String using string API?
+	// Sort the String without using String API?
+	// How to find largest element in an array with index and value ?
+	// Sort integer array using bubble sort in java?
+	// Check armstrong number or not?
+	// Multiply 2 Matrices
+	// Explain return type in java
+	// Can we call Sub class methods using super class object?
+	// Can we Override private methods ?
+
 }
