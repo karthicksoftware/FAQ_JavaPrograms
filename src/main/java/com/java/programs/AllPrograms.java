3e6610716205f643ddf673bc502f0d84ec575975
karunachalam <karunachalam@pch.com>
2018-04-05 06:23:43 -0400

    Adding updates

--- src/main/java/com/java/programs/AllPrograms.java
+++ src/main/java/com/java/programs/AllPrograms.java
@@ -1,11 +1,13 @@
 package com.java.programs;
 
+import java.util.Scanner;
+
 import org.testng.annotations.Test;
 
 public class AllPrograms {
 
 	// Print prime numbers?
-	@Test(enabled = true)
+	@Test(enabled = false)
 	public void printPrimeNumbers() {
 		//Prime numbers below 100
 		int flag = 0;
@@ -24,9 +26,66 @@ public class AllPrograms {
 	}
 	
 	// Basic Java program to find area and perimeter of circle
+	@Test(enabled = false)
+	public void perimeterOfCircle() {
+		double pi = Math.PI;
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter Radius of the Circle : ");
+		int radius = in.nextInt();
+		System.out.println("Perimeter or Circumference of the Circle is : "+ (2*pi*radius));
+	}
+	
 	// Print 1 to 10 without using loops
+	int i = 1;
+	@Test(enabled = false)
+	public void printWithoutLoops() {
+		if(i <= 10){
+			System.out.println(i);
+			i++;
+			printWithoutLoops();
+		}		
+	}
+	
 	// Print numbers in pyramid shape?
+	@Test(enabled = false)
+	public void printPyramidShape() {
+		
+	}
+	
 	// Add 2 Matrices
+	@Test(enabled = false)
+	public void addTwoMatrices() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter number of Rows : ");
+		int numberOfRows = input.nextInt();
+		System.out.print("Enter number of Columns : ");
+		int numberOfCols = input.nextInt();
+		int firstMatrix[][] = new int[numberOfRows][numberOfCols];
+		int secondMatrix[][] = new int[numberOfRows][numberOfCols];
+		int sumMatrix[][] = new int[numberOfRows][numberOfCols];
+		System.out.println("Enter the values in first matrix:");
+		for(int row = 0; row < numberOfRows; row++){
+			for(int col = 0; col < numberOfCols; col++){
+				firstMatrix[row][col] = input.nextInt();
+			}
+		}
+		System.out.println("Enter the values in second matrix:");
+		for(int row = 0; row < numberOfRows; row++){
+			for(int col = 0; col < numberOfCols; col++){
+				secondMatrix[row][col] = input.nextInt();
+			}
+		}
+		System.out.println("Sum of two matrices are:");
+		for(int row = 0; row < numberOfRows; row++){
+			for(int col = 0; col < numberOfCols; col++){
+				sumMatrix[row][col] = (firstMatrix[row][col] + secondMatrix[row][col]);
+				System.out.print(sumMatrix[row][col] + " ");
+			}
+			System.out.println();
+		}
+		input.close();
+	}
+	
 	// Basic Java Example Program to check even or add
 	// What happens if we place return statement in try catch blocks
 	// Write a java program to convert binary to decimal
