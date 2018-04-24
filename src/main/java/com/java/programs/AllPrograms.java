bd8250062a205df1056160f01495c17df3b4b538
karunachalam <karunachalam@pch.com>
2018-04-24 17:03:19 +0530

    adding updates

--- src/main/java/com/java/programs/AllPrograms.java
+++ src/main/java/com/java/programs/AllPrograms.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 import org.testng.annotations.Test;
 
@@ -12,22 +13,22 @@ public class AllPrograms {
 	// Print prime numbers?
 	@Test(enabled = false)
 	public void printPrimeNumbers() {
-		//Prime numbers below 100
+		// Prime numbers below 100
 		int flag = 0;
 		System.out.println(1);
-		for(int i=2; i<100; i++){
-			for(int j=1; j<=i; j++){
-				if(i%j == 0){
+		for (int i = 2; i < 100; i++) {
+			for (int j = 1; j <= i; j++) {
+				if (i % j == 0) {
 					flag++;
 				}
 			}
-			if(flag == 2)
+			if (flag == 2)
 				System.out.println(i);
 			flag = 0; // Its important to reset flag value to zero at any cost.
 		}
 
 	}
-	
+
 	// Basic Java program to find area and perimeter of circle
 	@Test(enabled = false)
 	public void perimeterOfCircle() {
@@ -35,26 +36,27 @@ public class AllPrograms {
 		Scanner in = new Scanner(System.in);
 		System.out.print("Enter Radius of the Circle : ");
 		int radius = in.nextInt();
-		System.out.println("Perimeter or Circumference of the Circle is : "+ (2*pi*radius));
+		System.out.println("Perimeter or Circumference of the Circle is : " + (2 * pi * radius));
 	}
-	
+
 	// Print 1 to 10 without using loops
 	int i = 1;
+
 	@Test(enabled = false)
 	public void printWithoutLoops() {
-		if(i <= 10){
+		if (i <= 10) {
 			System.out.println(i);
 			i++;
 			printWithoutLoops();
-		}		
+		}
 	}
-	
+
 	// Print numbers in pyramid shape?
 	@Test(enabled = false)
 	public void printPyramidShape() {
-		
+
 	}
-	
+
 	// Add 2 Matrices
 	@Test(enabled = false)
 	public void addTwoMatrices() {
@@ -67,20 +69,20 @@ public class AllPrograms {
 		int secondMatrix[][] = new int[numberOfRows][numberOfCols];
 		int sumMatrix[][] = new int[numberOfRows][numberOfCols];
 		System.out.println("Enter the values in first matrix:");
-		for(int row = 0; row < numberOfRows; row++){
-			for(int col = 0; col < numberOfCols; col++){
+		for (int row = 0; row < numberOfRows; row++) {
+			for (int col = 0; col < numberOfCols; col++) {
 				firstMatrix[row][col] = input.nextInt();
 			}
 		}
 		System.out.println("Enter the values in second matrix:");
-		for(int row = 0; row < numberOfRows; row++){
-			for(int col = 0; col < numberOfCols; col++){
+		for (int row = 0; row < numberOfRows; row++) {
+			for (int col = 0; col < numberOfCols; col++) {
 				secondMatrix[row][col] = input.nextInt();
 			}
 		}
 		System.out.println("Sum of two matrices are:");
-		for(int row = 0; row < numberOfRows; row++){
-			for(int col = 0; col < numberOfCols; col++){
+		for (int row = 0; row < numberOfRows; row++) {
+			for (int col = 0; col < numberOfCols; col++) {
 				sumMatrix[row][col] = (firstMatrix[row][col] + secondMatrix[row][col]);
 				System.out.print(sumMatrix[row][col] + " ");
 			}
@@ -88,27 +90,29 @@ public class AllPrograms {
 		}
 		input.close();
 	}
-	
+
 	// Basic Java Example Program to check even or add
 	@Test(enabled = false)
 	public void isOddOrEven() {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter number:");
 		int num = input.nextInt();
-		if(num % 2 == 0)
+		if (num % 2 == 0)
 			System.out.println("Even number");
 		else
 			System.out.println("Odd number");
 	}
-	
+
 	// What happens if we place return statement in try catch blocks
-		/*
-		 * If we have return statement in try, then return statement should be called at the end of the method as well,
-		 * Or else compile time error should be displayed.
-		 * If we have return statement in try and catch, then no need to specify another one return statement at the end of the method.
-		 * If we have return statement before any line inside method, then unreachable code warning should be shown.
-		 * */
-	
+	/*
+	 * If we have return statement in try, then return statement should be
+	 * called at the end of the method as well, Or else compile time error
+	 * should be displayed. If we have return statement in try and catch, then
+	 * no need to specify another one return statement at the end of the method.
+	 * If we have return statement before any line inside method, then
+	 * unreachable code warning should be shown.
+	 */
+
 	// Write a java program to convert binary to decimal
 	@Test(enabled = false)
 	public void convertBinaryToDecimal() {
@@ -118,7 +122,7 @@ public class AllPrograms {
 		int rem = 0;
 		int result = 0;
 		int power = 0;
-		while( value != 0 ){
+		while (value != 0) {
 			rem = value % 10;
 			result = (int) (result + (rem * (Math.pow(2, power))));
 			System.out.println(result);
@@ -127,7 +131,7 @@ public class AllPrograms {
 		}
 		input.close();
 	}
-	
+
 	// Java Program to convert Decimal to Binary
 	@Test(enabled = false)
 	public void convertDecimalToBinary() {
@@ -135,8 +139,8 @@ public class AllPrograms {
 		System.out.println("Enter decimal number:");
 		int value = input.nextInt();
 		List<Integer> result = new ArrayList<Integer>();
-		while(value != 0){
-			result.add(value%2);
+		while (value != 0) {
+			result.add(value % 2);
 			value = value / 2;
 		}
 		Collections.reverse(result);
@@ -144,23 +148,24 @@ public class AllPrograms {
 		System.out.println();
 		input.close();
 	}
-	
+
 	// Interfaces allows constructors?
 	/* No. */
-	
+
 	// Can we create private constructor in java
-	/*Yes, it is to restrict objects to be created outside the class*/
-	
+	/* Yes, it is to restrict objects to be created outside the class */
+
 	// Java interview Questions on main() method
 	/**
-	 * Main method should be static because it will be accessed without object reference.
-	 * Main method should be declared public, because it should be visible to every members in the package.
-	 * Main method can't return any types, thus it should be void always
-	 * Main method should be given with param of array type to driver data from command line.
+	 * Main method should be static because it will be accessed without object
+	 * reference. Main method should be declared public, because it should be
+	 * visible to every members in the package. Main method can't return any
+	 * types, thus it should be void always Main method should be given with
+	 * param of array type to driver data from command line.
 	 */
-	
+
 	// Explain java data types with example programs
-	
+
 	// Swap two numbers without using third variable in java
 	@Test(enabled = false)
 	public void swapNumbersWithoutThirdVariable() {
@@ -170,16 +175,16 @@ public class AllPrograms {
 		System.out.println("Enter number:");
 		int b = input.nextInt();
 		System.out.println("Before swapping,");
-		System.out.println("A = "+a);
-		System.out.println("B = "+b);
+		System.out.println("A = " + a);
+		System.out.println("B = " + b);
 		System.out.println("After swapping,");
 		a = a + b;
 		b = a - b;
 		a = a - b;
-		System.out.println("A = "+a);
-		System.out.println("B = "+b);		
+		System.out.println("A = " + a);
+		System.out.println("B = " + b);
 	}
-	
+
 	// Find sum of digits in java
 	@Test(enabled = false)
 	public void sumOfDigits() {
@@ -188,26 +193,26 @@ public class AllPrograms {
 		int a = input.nextInt();
 		int result = 0;
 		int rem = 0;
-		while(a != 0){
+		while (a != 0) {
 			rem = a % 10;
 			result += rem;
 			a = a / 10;
 		}
 		System.out.println(result);
 	}
-	
+
 	// Check Even or Odd without using modulus and division
 	@Test(enabled = false)
 	public void evenOrOddWithoutModuDiv() {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter number:");
 		int value = input.nextInt();
-		if((value & 1) == 0)
-			System.out.println("Even") ;
+		if ((value & 1) == 0)
+			System.out.println("Even");
 		else
 			System.out.println("Odd");
 	}
-	
+
 	// String Reverse Without using String API
 	@Test(enabled = false)
 	public void reverseStringWithoutAPI() {
@@ -216,29 +221,29 @@ public class AllPrograms {
 		String value = input.nextLine();
 		char[] result = new char[value.toCharArray().length];
 		for (int i = 0; i < value.toCharArray().length; i++) {
-			result[i] = value.charAt(value.toCharArray().length - 1 -i);
+			result[i] = value.charAt(value.toCharArray().length - 1 - i);
 		}
 		System.out.println(result);
 	}
-	
+
 	@Test(enabled = false)
 	public void getPow() {
-		System.out.println(pow(12,7));
+		System.out.println(pow(12, 7));
 		System.out.println(Math.pow(12, 7));
 	}
-	
-	double pow(double base, double power){
+
+	double pow(double base, double power) {
 		double result = 1;
 		int itr = 0;
-		while (itr < power){
+		while (itr < power) {
 			result = result * base;
 			itr++;
 		}
 		return result;
 	}
-	
+
 	// Check string is palindrome or not?
-	@Test(enabled = true)
+	@Test(enabled = false)
 	public void findPalindrome() {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter name:");
@@ -248,16 +253,54 @@ public class AllPrograms {
 		for (int i = 0; i < actual.length; i++) {
 			expected[i] = actual[actual.length - i - 1];
 		}
-		if(String.valueOf(expected).equals(String.valueOf(actual)))
+		if (String.valueOf(expected).equals(String.valueOf(actual)))
 			System.out.println("Is Palindrome");
 		else
 			System.out.println("Is Not a Palindrome");
 	}
-	
+
 	// Reverse a number in java?
+	@Test(enabled = false)
+	public void reverseNumber() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter name:");
+		int value = input.nextInt();
+		while (value != 0) {
+			int lastNum = value % 10;
+			System.out.print(lastNum);
+			value = value / 10;
+		}
+	}
+
 	// Fibonacci series with Recursive?
+	
 	// Fibonacci series without using Recursive?
-	// Sort the String using string API? 
+	@Test(enabled = false)
+	public void fibo() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter name:");
+		int value = input.nextInt();
+		int a = 0;
+		int b = 1;
+		int res = 0;
+		int itr = 0;
+		System.out.print(a);
+		System.out.print(" ");
+		System.out.print(b);
+		while(itr < value){
+			res = a + b;
+			a = b;
+			System.out.print(" ");
+			b = res;
+			itr++;
+			System.out.print(res);
+		}
+		System.out.println();
+	}
+
+	// Sort the String using string API?
+	
+	
 	// Sort the String without using String API?
 	// How to find largest element in an array with index and value ?
 	// Sort integer array using bubble sort in java?
